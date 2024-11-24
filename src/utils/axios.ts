a0974c3ef28f1dc0fce58a35d6cee4af198ca4d5
Alok Yadav <yalok6321@gmail.com>
2024-11-24 11:43:04 +0530

    axios config

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/api/v1";
+axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}/api/v1`;
 axios.defaults.withCredentials = true;
 
 export default axios;
